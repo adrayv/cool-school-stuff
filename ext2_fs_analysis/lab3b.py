63c4b5d74215715b23c7bb804e7e6b39ba8cd495
Destin Anthony Venturina Raymundo <destin@lnxsrv09.seas.ucla.edu>
2017-06-04 15:52:41 -0700

    able to open file

--- ext2_fs_analysis/lab3b.py
+++ ext2_fs_analysis/lab3b.py
@@ -1,5 +1,19 @@
 import sys
 
+def openFile():
+	if len(sys.argv) != 2:
+		error_message("incorrect num arguments", 1)
+	else:
+		try:
+			file = open(sys.argv[1], 'r')
+		except IOError:
+			error_message("file does not exist", 1)
+		return file
+
+def error_message(message, rc):
+	print message
+	sys.exit(rc)
+
 if __name__ == "__main__":
-	print sys.argv
-	print len(sys.argv)
+	# get file and error check
+	file = openFile()
